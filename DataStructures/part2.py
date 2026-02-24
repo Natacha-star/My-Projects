ad719326d872599fbb9c69b89d464e832670e346
Iradukunda Natacha <natachairakunda@gmail.com>
2026-02-24 14:26:49 +0200

    slightly changed part2

--- DataStructures/part2.py
+++ DataStructures/part2.py
@@ -5,12 +5,18 @@ matrix_B = [[5, 6], [7, 8]]
 # Initialize a 2x2 matrix for the result with zeros
 result = [[0, 0], [0, 0]]
 
+
 # Addition logic
 for i in range(2):        # Loop through rows
     for j in range(2):    # Loop through columns
         result[i][j] = matrix_A[i][j] + matrix_B[i][j]
+        if result[i][j] > 10:
+            result[i][j] = 10
 
 # Display result
 print("Resulting Matrix:")
 for row in result:
+    print(row)
+for row in result:
+    
     print(row)
